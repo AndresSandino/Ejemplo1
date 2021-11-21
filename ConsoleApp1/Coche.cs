b03d23f90e48af54ea58c39c4449d3b65e2c91bf
Andrés Sandino <andressandino22@gmail.com>
2021-11-21 12:59:25 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Coche.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Coche
+    {
+        public string nombre { get; set; }
+        public string color { get; set; }
+        public string placa { get; set; }
+        public int km { get; set; }
+
+        public void arrancar(int kmHora)
+        {
+            km = kmHora;
+        }
+        public void frenar()
+        {
+            km = 0;
+        }
+        public void avanzar()
+        {
+            km++;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche Ferrari = new Coche();
+            Ferrari.nombre = "Ferrari";
+            Ferrari.color = "Rojo";
+            Ferrari.placa = "WFQ-481";
+            Ferrari.km = 300;
+
+            Coche Logan = new Coche();
+            Logan.nombre = "logan";
+            Logan.color = "Negro";
+            Logan.placa = "MQI_926";
+            Logan.km = 180;
+
+            Logan.arrancar(50);
+            Console.WriteLine("Velocidad actual: " + Logan.km);
+            Logan.avanzar();
+            Console.WriteLine("Velocidad actual: " + Logan.km);
+            Logan.frenar();
+            Console.WriteLine("Velocidad actual: " + Logan.km);
+            Logan.avanzar();
+            Console.WriteLine("Velocidad actual: " + Logan.km);
+        }
+    }
+}
